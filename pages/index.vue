a2bc6c6a09140e39b47c27e3022e471b9f0832e2
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2025-03-09 12:03:02 +0100

    [ADD]-Ajout de l'API
    -Affiche sur la page index, 10 pokémons et leur noms à l'aide de l'API

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: pokemons } = useFetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+</script>
+
+<template>
+  <div>
+    <h1>Pokédex</h1>
+    <ul>
+      <li v-for="pokemon in pokemons?.results" :key="pokemon.name">
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
