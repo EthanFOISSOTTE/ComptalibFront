d5d9daa823207f7d298eb1006a2cada5cab24990
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2025-03-13 13:02:01 +0100

    [EDIT]-Utilisation de Pinia pour récupérer l'équipe pokémon

--- pages/pokemon/[name].vue
+++ pages/pokemon/[name].vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, useRoute } from '#imports';
+import { ref, useRoute, onMounted } from '#imports';
+import { useTeamStore } from '~/stores/team';
 import '../../assets/pokemon/stylePokemon.css';
 
 const pokemonDetail = ref(null);
@@ -7,6 +8,8 @@ const cache = new Map();
 
 const route = useRoute();
 const pokemonName = route.params.name;
+
+const teamStore = useTeamStore();
 
 const fetchPokemonDetails = async () => {
   if (cache.has(pokemonName)) {
@@ -24,6 +27,7 @@ const fetchPokemonDetails = async () => {
   });
 
   const pokemon = {
+    id: data.id,
     name: data.name,
     sprite: data.sprites.front_default,
     types: types,
@@ -36,22 +40,54 @@ const fetchPokemonDetails = async () => {
   pokemonDetail.value = pokemon;
 };
 
-fetchPokemonDetails();
+onMounted(fetchPokemonDetails);
 </script>
 
 <template>
   <body>
-    <div v-if="pokemonDetail" class="pokemon-detail">
-      <h1>{{ pokemonDetail.name }}</h1>
-      <img :src="pokemonDetail.sprite">
-      <div><strong>Types:</strong> {{ pokemonDetail.types }}</div>
-      <div><strong>Hauteur:</strong> {{ pokemonDetail.height }}dm</div>
-      <div><strong>Poids:</strong> {{ pokemonDetail.weight }}</div>
-      <div><strong>Statistiques:</strong></div>
-      <ul>
-        <li v-for="(stat, key) in pokemonDetail.stats" :key="key">{{ key }}: {{ stat }}</li>
-      </ul>
-      <NuxtLink to="/">Retour à la liste</NuxtLink>
+    <div class="nav-container">
+      <div class="nav-title"><h1>Pokédex Comptalib</h1></div>
+      <div class="nav-idk">
+        <img src="../../assets/images/pokeapi.png">
+      </div>
+    </div>
+
+    <div v-if="pokemonDetail" class="name-container">
+      <h1 class="name">{{ pokemonDetail.name }}</h1>
+    </div>
+
+    <div class="main-content">
+      <div v-if="pokemonDetail" class="pokemon-detail">
+        <div class="pokemon-container-info">
+          <img :src="pokemonDetail.sprite">
+          <div class="id-type-container">
+            <div><strong>Identifiant:</strong> {{ pokemonDetail.id }}</div>
+            <div><strong>Types:</strong> {{ pokemonDetail.types }}</div>
+          </div>
+          <div class="height-weight-container">
+            <div><strong>Hauteur:</strong> {{ pokemonDetail.height }} dm</div>
+            <div><strong>Poids:</strong> {{ pokemonDetail.weight }}</div>
+          </div>
+
+          <div><strong>Statistiques:</strong></div>
+          <ul>
+            <li v-for="(stat, key) in pokemonDetail.stats" :key="key">{{ key }}: {{ stat }}</li>
+          </ul>
+          <NuxtLink to="/">Retour à la liste</NuxtLink>
+        </div>
+      </div>
+
+      <div class="team-container">
+        <p style="padding-top: 10px; padding-bottom: 10px">Votre Équipe Pokémon</p>
+        <UDivider />
+        <div v-if="teamStore.team.length === 0">Aucun Pokémon dans l'équipe</div>
+        <ul>
+          <li v-for="(pokemon, index) in teamStore.team" :key="index">
+            <img :src="pokemon.sprite"> {{ pokemon.name }}
+            <UButton @click="teamStore.removePokemon(index)" color="red" label="Retirer" />
+          </li>
+        </ul>
+      </div>
     </div>
   </body>
 
